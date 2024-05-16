3687b0792470d8a94a5d42abcdbdd116db75f2c8
harshithakurra8 <harshitha9.kr@gmail.com>
2024-05-16 22:26:25 +0530

    bool
    True
    (or)
    False
    
    In python, any integer '0' is considered as a false value,an non-zero integer is considered as a true value.
    
    A string is empty is considered as false.
    
    A float like 0.0 is considered as false.

---
+++ booleans.py
@@ -0,0 +1,12 @@
+print(0,bool(0))
+print(1,bool(1))
+
+
+quantity=21
+if quantity:
+    print("Thanks for the purchase!")
+else:
+    print("The purchase should be atleast 1")
+
+strings=""
+print(bool(strings))
