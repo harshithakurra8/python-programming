cbc2fab430a82a6ab2e6ee02cadca518516a5458
harshithakurra8 <harshitha9.kr@gmail.com>
2024-05-18 11:43:50 +0530

    decision control
    elif statement,
    Comparision operatiors,
    Joining Condition ,
    Ternary operators
    
    Joining Condition
    And,or,not,in keywors

--- decison control.py
+++ decison control.py
@@ -55,14 +55,3 @@ if not num>0:
 num=10.1878
 num=int(num) if num.is_integer()else num
 print(num)
-
-#In a certain company, you'll get car insurance if an employee meets the following conditions -
-#The employee must be regular.
-#The employee is a male and above 30 years of age.
-#The employee is a female and above 25 years of age.
-#Define a function is_insured() which will receive employee data as a dictionary like
-def is_insured(employee):
-    if employee["is_regular"] and ((employee["gender"] == "Male" and employee["age"] > 30) or (employee["gender"] == "Female" and employee["age"] > 25)):
-        print(f"{employee['name']} is insured")
-    else:
-        print(f"{employee['name']} is not insured")
