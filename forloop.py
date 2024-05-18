f4250aa05f780ada319cc829242d50c985ec77ea
harshithakurra8 <harshitha9.kr@gmail.com>
2024-05-18 17:53:14 +0530

    forloop

---
+++ forloop.py
@@ -0,0 +1,16 @@
+lst=["milk","bread","egg"]
+
+index=0
+while index!=len(lst):
+    print(lst[index])
+    index+=1
+
+
+lst=["milk","bread","egg"]
+for item in lst:
+    print(item)
+
+    
+lst=["milk","bread","egg"]
+for index in range(0,3):
+    print(index, lst[index])
